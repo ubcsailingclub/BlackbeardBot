8288951584070991b508e463f5b6b245f059ed58
hdignoes <45015756+hdignoes@users.noreply.github.com>
2026-01-20 22:47:12 -0800

    Clarify comment for VERIFY_CHANNEL_NAME

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("DISCORD_TOKEN", "")
 WA_API_KEY = os.getenv("WA_API_KEY", "")
 WA_ACCOUNT_ID = int(os.getenv("WA_ACCOUNT_ID", "0"))
 
-VERIFY_CHANNEL_NAME = "get-verified"  # Discord server used to get verified
+VERIFY_CHANNEL_NAME = "get-verified"  # Discord server channel used to get verified
 VERIFY_MESSAGE_STATE_FILE = "data/verify_message.json"
 
 WA_API_VERSION = "v2.1"
